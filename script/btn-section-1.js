45c4af7ac76a9df17af7bf1b5a92f849fb7b4db2
Evg <chilicklic@mail.ru>
2019-07-23 21:28:57 +0600

    Adaptive size 640px site
    — Resolution 640px section 1-5;
    — in classes rename tezt in text;

---
+++ script/btn-section-1.js
@@ -0,0 +1,11 @@
+function openMenuSection1() {
+    document.getElementById("headers-adaptive").style.display = "flex";
+    document.getElementById("nav-adapt").style.display = "none";
+    document.getElementById("btn-adapt").style.display = "none";
+}
+
+function closeMenuSection1() {
+    document.getElementById("headers-adaptive").style.display = "none";
+    document.getElementById("nav-adapt").style.display = "block";
+    document.getElementById("btn-adapt").style.display = "block";
+}
