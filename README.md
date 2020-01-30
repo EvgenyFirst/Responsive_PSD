b8fd7d295b1d41506b4f129eb2f91a1b0fbf0893
EvgenyFirst <>
2020-01-30 19:47:40 +0600

    Restert
    — Restarting site writing;
    —  Added:
       —  The site code in the html file;
       —  In resolution 320px sections 1 and 2 are written, and also the Navigation menu in improvisations is added;
    — Added comment to file README.md about improvisationюn.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 https://wp-query.ru/luchshie-besplatnye-psd-makety-sajtov-dlya-verstki/
 
 http://www.blazrobar.com/2013/free-psd-files-and-ui-kits/responsive-psd-template/
+
+В верстку сайта добавлена импровизация с меню Navigation на 320px и 640px.
--- script/btn-section-1.js
+++ script/btn-section-1.js
@@ -1,11 +1,11 @@
 function openMenuSection1() {
-    document.getElementById("headers-adaptive").style.display = "flex";
-    document.getElementById("nav-adapt").style.display = "none";
-    document.getElementById("btn-adapt").style.display = "none";
+    document.querySelector(".section-1__main-menu").style.display = "block";
+    document.querySelector(".main-nav__btn-close").style.display = "block";
+    document.querySelector(".section-1__main-nav").style.display = "none";
 }
 
 function closeMenuSection1() {
-    document.getElementById("headers-adaptive").style.display = "none";
-    document.getElementById("nav-adapt").style.display = "block";
-    document.getElementById("btn-adapt").style.display = "block";
+    document.querySelector(".section-1__main-menu").style.display = "none";
+    document.querySelector(".main-nav__btn-close").style.display = "none";
+    document.querySelector(".section-1__main-nav").style.display = "flex";
 }
